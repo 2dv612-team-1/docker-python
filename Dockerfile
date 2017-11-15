f6aacab27ff22709639ddf524c71c17fbd57197c
slaxku <Rickardthom@outlook.com>
2017-11-15 08:26:28 +0100

    experiments on accessing mongodb through flask

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.6
 ADD . /app
 WORKDIR /app
+COPY requirements.txt /app/
 RUN pip install -r requirements.txt
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, jsonify, redirect, url_for, request, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -7,25 +7,46 @@ app = Flask(__name__)
 client = MongoClient('mongodb:27017')
 db = client.tododb
 
-@app.route('/')
-def todo():
-
-    _items = db.tododb.find()
-    items = [item for item in _items]
-
-    return render_template('todo.html', items=items)
+representatives = db.representatives
 
 
-@app.route('/new', methods=['POST'])
-def new():
+@app.route('/representatives', methods=['GET'])
+def getRepresentatives():
+	db_reps = db.representatives
+	
+	json_reps = []
 
-    item_doc = {
-        'name': request.form['name'],
-        'description': request.form['description']
-    }
-    db.tododb.insert_one(item_doc)
+	for rep in db_reps.find():
+		json_reps.append({'name' : rep['name'], 'company' : rep['company']})
+	
+	return jsonify({'result' : json_reps})
 
-    return redirect(url_for('todo'))
+@app.route('/test', methods=['GET'])
+def test():
+	name = 'Andersch Von Monegrabben'
+	company = 'EA'
+
+	query = representatives.find_one({'name' : name})
+
+	if query:
+		out = {'name' : query['name'], 'company' : query['company']}
+	else:
+		representatives.insert({'name' : name, 'company' : company})
+		out = name + " added"
+	
+	return jsonify({'result' : out})
+
+@app.route('/representatives/<name>', methods=['GET'])
+def getRepresentativeByName(name):
+	db_reps = db.representatives
+	query = representatives.find_one({'name' : name})
+	
+	if query:
+		out = {'name' : query['name'], 'company' : query['company']}
+	else:
+		out = 'No results found'
+
+	return jsonify({'result' : out})
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True, port=80)
